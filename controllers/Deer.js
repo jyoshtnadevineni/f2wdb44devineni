aacd407de172811d736a6ecd95586ffe8144b0c5
jyoshtnadevineni <111612663+jyoshtnadevineni@users.noreply.github.com>
2022-11-16 21:59:14 -0600

    Updated files added files to repo

--- controllers/Deer.js
+++ controllers/Deer.js
@@ -14,72 +14,20 @@ exports.Deer_detail = async function(req, res) {
         res.send(`{"error": document for id ${req.params.id} not found`); 
     } 
 }; 
-// HandleDeer create on POST.
 
-// exports.Deer_create_post = async function(req, res) {
-
-//     console.log(req.body)
-
-//     let document = newDeer();
-
-//     document.Deer_Name = req.body.Deer_Name;
-
-//     document.Deer_size = req.body.Deer_size;
-
-//     document.Deer_type= req.body.Deer_type;
-//      try{
-
-//         let result = await document.save();
-
-//         res.send(result);
-
-//     }
-
-//     catch(err){
-
-//         res.status(500);
-
-//         res.send(`{"error": ${err}}`);
-
-//     }  
-
-// };
-
-// Handle Costume delete form on DELETE.
+// Handle Deer delete form on DELETE.
 exports.Deer_delete = function(req, res){
  res.send('NOT IMPLEMENTED: Deer delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
+// Handle Deer update form on PUT.
 
-// exports.Deer_update_put = function(req, res){
-//  res.send('NOT IMPLEMENTED: Deer update PUT' + req.params.id);
-// };
-// exports.Deer_list = async function(req, res) {
-
-//     try{
-
-//         theDeer= await theDeer.find();
-
-//         res.send(theDeer);
-
-//     }
-
-//     catch(err){
-
-//         res.status(500);
-
-//         res.send(`{"error": ${err}}`);
-
-//     }  
-
-// };
 exports.Deer_update_put = async function(req, res) { 
     console.log(`update on id ${req.params.id} with body 
 ${JSON.stringify(req.body)}`) 
     try { 
         let toUpdate = await Deer.findById( req.params.id) 
         // Do updates of properties 
-        if(req.body.Deer_name) toUpdate.Deer_name = req.body.Deer_name; 
+        if(req.body.Deer_Name) toUpdate.Deer_Name = req.body.Deer_Name; 
         if(req.body.Deer_size) toUpdate.Deer_size = req.body.Deer_size; 
         if(req.body.Deer_type) toUpdate.Deer_type = req.body.Deer_type; 
         let result = await toUpdate.save(); 
@@ -137,3 +85,67 @@ exports.Deer_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }
+// Handle Deer delete on DELETE.
+exports.Deer_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await Deer.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+// Handle a show one view with id specified by query
+exports.Deer_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await Deer.findById( req.query.id)
+    res.render('Deerdetail',
+    { title: 'Deer Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for creating a Deer.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Deer_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Deercreate', { title: 'Deer Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle building the view for updating a Deer.
+// query provides the id
+exports.Deer_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Deer.findById(req.query.id)
+    res.render('Deerupdate', { title: 'Deer Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+// Handle a delete one view with id from query
+exports.Deer_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Deer.findById(req.query.id)
+    res.render('Deerdelete', { title: 'Deer Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/Deer.js
+++ routes/Deer.js
@@ -17,4 +17,14 @@ router.get('/', Deer_controlers.Deer_view_all_Page );
  //router.put('/Deer/:id', Deer_controlers.Deer_update_put);
 
 module.exports = router;
+// GET request for one Deer.
+router.get('/Deer/:id', Deer_controlers.Deer_detail);
+/* GET detail Deer page */
+router.get('/detail', Deer_controlers.Deer_view_one_Page);
+/* GET create Deer page */
+router.get('/create', Deer_controlers.Deer_create_Page);
+/* GET create update page */
+router.get('/update', Deer_controlers.Deer_update_Page);
+/* GET delete Deer page */
+router.get('/delete', Deer_controlers.Deer_delete_Page);
 
